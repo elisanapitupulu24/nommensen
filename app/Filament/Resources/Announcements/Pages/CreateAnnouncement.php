cb68457b9092359378f60811ea966cbed30e4b67
elisanapitupulu24 <elisanapitupulu2401@gmail.com>
2026-06-29 08:31:43 +0700

    feat: customize Announcement and News resources with auto slug and user relation

--- app/Filament/Resources/Announcements/Pages/CreateAnnouncement.php
+++ app/Filament/Resources/Announcements/Pages/CreateAnnouncement.php
@@ -8,4 +8,16 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateAnnouncement extends CreateRecord
 {
     protected static string $resource = AnnouncementResource::class;
+      /**
+     * Hook yang dipanggil tepat sebelum data disimpan ke database.
+     * Di sini kita mengisi slug (otomatis dari judul) dan users_id
+     * (otomatis dari admin yang sedang login).
+     */
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['slug']     = Str::slug($data['title']) . '-' . time();
+        $data['users_id'] = Auth::id();
+
+        return $data;
+    
 }
--- app/Filament/Resources/Students/Schemas/StudentForm.php
+++ app/Filament/Resources/Students/Schemas/StudentForm.php
@@ -6,40 +6,40 @@ use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
 use Filament\Schemas\Schema;
+use Livewire\Features\SupportFileUploads\FileUploadConfiguration;
 
 class StudentForm
 {
-    public static function configure(Schema $schema): Schema
-    {
-        return $schema
-            ->components([
-               TextInput::make('namalengkap')
+  public static function form(Form $form): Form
+{
+    return $form
+        ->schema([
+          TextInput::make('namalengkap')
                 ->label('Nama Lengkap')
                 ->required()
                 ->maxLength(255)
                 ->placeholder('contoh: Muhammad Rizky Pratama'),
 
-              TextInput::make('namapanggilan')
+         TextInput::make('namapanggilan')
                 ->label('Nama Panggilan')
                 ->required()
                 ->maxLength(255)
                 ->placeholder('contoh: Rizky'),
 
-             TextInput::make('email')
+           TextInput::make('email')
                 ->label('Email')
                 ->email()
                 ->required()
                 ->maxLength(255)
                 ->placeholder('contoh: rizky.pratama@gmail.com'),
 
-             TextInput::make('nomor_hp')
+            TextInput::make('nomor_hp')
                 ->label('Nomor HP')
                 ->required()
                 ->maxLength(15)
                 ->placeholder('contoh: 081234567890')
                 ->helperText('Maksimal 15 digit, format 08xxxxxxxxxx.'),
-
-             Select::make('jalur')
+           Select::make('jalur')
                 ->label('Jalur Masuk')
                 ->options([
                     'Reguler'  => 'Reguler',
@@ -49,19 +49,19 @@ class StudentForm
                 ->required()
                 ->placeholder('Pilih jalur masuk'),
 
-             TextInput::make('programstudi_1')
+            TextInput::make('programstudi_1')
                 ->label('Pilihan Program Studi 1')
                 ->required()
                 ->maxLength(255)
                 ->placeholder('contoh: D3 Teknik Komputer'),
 
-             TextInput::make('programstudi_2')
+          TextInput::make('programstudi_2')
                 ->label('Pilihan Program Studi 2')
                 ->required()
                 ->maxLength(255)
                 ->placeholder('contoh: D3 Sistem Informasi'),
 
-             FileUpload::make('image')
+          FileUpload::make('image')
                 ->label('Foto')
                 ->image()
                 ->directory('students')
@@ -74,5 +74,5 @@ class StudentForm
         ])
         ->columns(2);
 }
-    }
-
+}
+    
